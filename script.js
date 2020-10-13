dd3bf0b1a228098e5c1b1cffc5f511a5b50e777b
Anuj Verma <68846777+anujvdev2020@users.noreply.github.com>
2020-10-13 12:31:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ window.addEventListener('keydown',function(e){
 		key.classList.add('playing')
 		console.log(key)
 	})
+
+
+window.addEventListener('click',function(e){
+		const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`)
+		const key=document.querySelector(`.key[data-key="${e.keyCode}"]`)
+		if(!audio){
+			return;///stop the function from runnnig
+		}
+		audio.currentTime=0;///rewind to start
+		audio.play();
+		key.classList.add('playing')
+		console.log(key)
+	})
